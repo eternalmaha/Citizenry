02005412fb1a4595d0284b8a568a3872e5a5a098
Maha <maha.ahmed4peace@gmail.com>
2024-06-06 15:35:15 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <section>
+      <h1>Citizeny</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
